a122117e66e1acd5810af6b6033918da79e00061
Cassandra Perez <cassandraenriq@icloud.com>
2020-04-24 22:10:53 -0500

    question update

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ var questions = [
         (B) Booleans\n (C)Alerts\n (D) Numbers",
         answer: "D"
     }, 
+    {
+        prompt: "The Condition in and if/else statement is enclosed within ___:\n (A) Quotes\n\
+        (B) Curly Brackets \n (C)Parentheses\n (D) Square Brackets",
+        answer: "A"
+    }, 
+
 ]
