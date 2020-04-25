051b2171912d6b6a43c93b53203b4992e0fe1cc4
Cassandra Perez <cassandraenriq@icloud.com>
2020-04-24 21:46:59 -0500

    added content and styled it

---
+++ script.js
@@ -0,0 +1,7 @@
+var questions = [
+    {
+        prompt: "Commonly used data types DO NOT include:\n (A) Strings\n\
+        (B) Booleans\n (C)Alerts\n (D) Numbers",
+        answer: "D"
+    }, 
+]
