d7f4b99b7594236e14d99ffeced636332b97f865
Cassandra Perez <cassandraenriq@icloud.com>
2020-04-25 01:00:25 -0500

    button clicks

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 
+
+var generateBtn = document.querySelector("#Start Quiz");
+
+
+
+
 //array of question programs
-var questions = [
+//var questions = [
 
-    {
-        prompt: "Commonly used data types DO NOT include:\n (A) Strings\n\
-        (B) Booleans\n (C)Alerts\n (D) Numbers",
-        answer: "D"
-    }, 
-    {
-        prompt: "The Condition in and if/else statement is enclosed within ___:\n (A) Quotes\n\
-        (B) Curly Brackets \n (C)Parentheses\n (D) Square Brackets",
-        answer: "A"
-    }, 
-    {
-        prompt: "Arrays in JavaScript can be used to store__:\n (A) Numbers and strings\n\
-        (B) Other\n (C)Booleans\n (D) All of the above",
-        answer: "D"
-    }, 
-    {
-        prompt: "String values must be enclosed within __ when being assigned to variables.:\n (A) Commas\n\
-        (B) Curly brackets\n (C)Quotation Marks\n (D) Parentheses",
-        answer: "C"
-    }, 
-    {
-        prompt: "A very useful tool used during development and debugging for printing content to the debugger is:\n (A) Javascript\n\
-        (B) terminal/bash\n (C)for loops\n (D) console.log",
-        answer: "C"
-    }, 
+   // {
+        //prompt: "Commonly used data types DO NOT include:\n (A) Strings\n\
+        //(B) Booleans\n (C)Alerts\n (D) Numbers",
+        //answer: "D"
+    //}, 
+    //{
+        //prompt: "The Condition in and if/else statement is enclosed within ___:\n (A) Quotes\n\
+        //(B) Curly Brackets \n (C)Parentheses\n (D) Square Brackets",
+        //answer: "A"
+    //},  
+    //{
+       // prompt: "Arrays in JavaScript can be used to store__:\n (A) Numbers and strings\n\
+        //(B) Other\n (C)Booleans\n (D) All of the above",
+       // answer: "D"
+   // }, 
+    //{
+       // (B) Curly brackets\n (C)Quotation Marks\n (D) Parentheses",
+       // answer: "C"
+   // }, 
+    //{
+       // prompt: "A very useful tool used during development and debugging for printing content to the debugger is:\n (A) Javascript\n\
+      //  (B) terminal/bash\n (C)for loops\n (D) console.log",
+      //  answer: "C"
+   // }, 
 
-]
-var score =0
+//]
+//var score =0
